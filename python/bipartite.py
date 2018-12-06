9ac1bc3969e006442bce328d157113c9293e1135
theazgra <theazgra@gmail.com>
2018-12-06 20:06:32 +0100

    Finished projection of bipartite graph.

---
+++ python/bipartite.py
@@ -0,0 +1,64 @@
+from matrix import Mat
+
+
+def load_bipartite():
+    lines = []
+
+    with open("D:/gitrepos/dataAnalysisCourse/data/actorsAndMovies.txt") as dataset:
+        lines = dataset.readlines()
+
+    labels = {}
+    edges = []
+    maxVertex = 0
+    readingEdges = False
+    for line in lines:
+        line = line.strip()
+        if line == "EDGES":
+            readingEdges = True
+            continue
+
+        lineValues = line.split(";")
+        if readingEdges:
+            a = int(lineValues[0]) - 1
+            b = int(lineValues[1]) - 1
+            if a > maxVertex:
+                maxVertex = a
+            if b > maxVertex:
+                maxVertex = b
+            edges.append((a, b))
+        else:
+            labels[int(lineValues[0])] = lineValues[1].replace('"', "")
+
+    rowCount = 10
+    values = [[0 for c in range(len(labels))] for r in range(rowCount)]
+
+    for edge in edges:
+        (a, b) = edge
+        values[a][b] = 1
+
+    bm = Mat(values, tuple(labels.values()))
+    return bm
+
+
+def main():
+    bipartiteMatrix = load_bipartite()
+    # print(bipartiteMatrix)
+    # bipartiteMatrix.print_labels()
+    (leftMat, rightMat) = bipartiteMatrix.one_mode_projection_for_bipartite_graph()
+
+    print("Matrix of actors. There is no connection.")
+    leftMat.print_labels()
+    print(leftMat)
+    print("Average degree: " + str(leftMat.get_average_degree()))
+
+    print("Matrix of movies")
+    print(rightMat)
+
+    print("Average degree: " + str(rightMat.get_average_degree()))
+    print("Average clustering coefficient: " +
+          str(rightMat.get_average_clustering_coefficients_for_vertices()))
+    print("Edge count: " + str(rightMat.get_edge_count()))
+
+
+if __name__ == "__main__":
+    main()
--- python/matrix.py
+++ python/matrix.py
@@ -1,11 +1,14 @@
 from itertools import groupby
 from collections import OrderedDict
+from queue import Queue
 from math import inf
 
+
 class Mat(object):
-    def __init__(self, values):
+    def __init__(self, values, labels=None):
         self.rowCount = len(values)
         self.colCount = len(values[0])
+        self.labels = labels
 
         for row in values:
             if (len(row)) != self.colCount:
@@ -17,8 +20,22 @@ class Mat(object):
         """
         @values will be enumerable with some enumerables in it.
         """
+
+    def get_label(self, vertex):
+        assert vertex < self.rowCount
+        if self.labels == None:
+            return ""
+        else:
+            return self.labels[vertex]
+
+    def print_labels(self):
+        if self.labels != None:
+            for r in range(self.rowCount):
+                print("Vertex: {}: {}".format(r, self.labels[r]))
+
     def set_all_values(self, value):
-        self.values =  tuple(tuple(value for cId in range(0, self.colCount)) for rId in range(0, self.rowCount))
+        self.values = tuple(tuple(value for cId in range(0, self.colCount))
+                            for rId in range(0, self.rowCount))
 
     def col(self, colIndex):
         return tuple(row[colIndex] for row in self.values)
@@ -57,17 +74,18 @@ class Mat(object):
         newValues = [[(self.values[rid][cid] - other.values[rid][cid])
                       for cid in range(0, self.colCount)] for rid in range(0, self.rowCount)]
         return Mat(newValues)
-    
+
     def transpose(self):
         """
         Fix for matrices, which aren't squared.
         """
-        newValues = tuple(tuple(self.values[cId][rId] for cId in range(0, self.colCount)) for rId in range(0, self.rowCount))
+        newValues = tuple(tuple(self.values[cId][rId] for cId in range(
+            0, self.colCount)) for rId in range(0, self.rowCount))
         return Mat(newValues)
-                
+
     def diagonal(self):
         return [self.values[rId][rId] for rId in range(0, self.rowCount)]
-    
+
     def get_degree_of_vertices(self):
         result = {}
         for vertex in range(0, self.rowCount):
@@ -98,8 +116,7 @@ class Mat(object):
             return Mat(newValues)
         if isinstance(other, Mat):
             return self._matrix_mul(other)
-    
-    
+
     def set_inf_where_no_edge(self):
         return Mat(tuple(tuple((self.values[rId][cId] if (self.values[rId][cId] > 0) else float('inf')) for cId in range(0, self.colCount)) for rId in range(0, self.rowCount)))
 
@@ -113,22 +130,24 @@ class Mat(object):
                     if tmpMat.values[i][j] > tmp:
                         tmpMat.values[i][j] = tmp
 
-        return self
-    
+        return tmpMat
+
     def closeness_centrality_for_vertices(self):
         vertexCount = self.rowCount
         result = []
 
         for rId in range(0, self.rowCount):
-            result.append(round(1 / (sum([self.values[rId][cId] for cId in range(0, self.colCount) if cId != rId]) / vertexCount), 7))
+            result.append(round(
+                1 / (sum([self.values[rId][cId] for cId in range(0, self.colCount) if cId != rId]) / vertexCount), 7))
 
         return result
 
     def get_neighbours(self, vertex):
         row = self.values[vertex]
-        neighbours = [ nId for nId in range(0, self.colCount) if row[nId] == 1 and nId != vertex]
+        neighbours = [nId for nId in range(
+            0, self.colCount) if row[nId] == 1 and nId != vertex]
         return neighbours
-    
+
     def get_edge_count(self):
         edgeCount = 0
         for r in range(self.rowCount):
@@ -147,7 +166,8 @@ class Mat(object):
 
     def get_average_clustering_coefficients_for_vertices(self):
         clusteringCoeffForVertices = self.get_clustering_coefficients_for_vertices()
-        result = sum(clusteringCoeffForVertices.values()) / len(clusteringCoeffForVertices)
+        result = sum(clusteringCoeffForVertices.values()) / \
+            len(clusteringCoeffForVertices)
         return result
 
     def get_clustering_coefficients_for_vertices(self, vertices=None):
@@ -157,17 +177,18 @@ class Mat(object):
         for vertex in vertices:
             vertexNeighbours = self.get_neighbours(vertex)
             neighbourCount = len(vertexNeighbours)
-            
+
             if neighbourCount > 1:
-                edgesBetweenNeighbours = self.get_edge_count_between_neighbours(vertexNeighbours)
-                clusteringCoefficien = (2.0 * edgesBetweenNeighbours) / (neighbourCount * (neighbourCount - 1))
-                result[vertex] = clusteringCoefficien    
+                edgesBetweenNeighbours = self.get_edge_count_between_neighbours(
+                    vertexNeighbours)
+                clusteringCoefficien = (
+                    2.0 * edgesBetweenNeighbours) / (neighbourCount * (neighbourCount - 1))
+                result[vertex] = clusteringCoefficien
             else:
                 result[vertex] = 0
-            
+
         return result
 
-    
     def get_vertices_grouped_by_degree(self):
         degreesDict = self.get_degree_of_vertices()
         result = {}
@@ -181,12 +202,13 @@ class Mat(object):
         groups = self.get_vertices_grouped_by_degree()
         result = {}
         for groupDegree in groups:
-            groupClusteringCoeffs = self.get_clustering_coefficients_for_vertices(groups[groupDegree])
+            groupClusteringCoeffs = self.get_clustering_coefficients_for_vertices(
+                groups[groupDegree])
             groupSize = len(groups[groupDegree])
             summedClusteringCoefficient = sum(groupClusteringCoeffs.values())
             groupAvg = summedClusteringCoefficient / float(groupSize)
             result[groupDegree] = groupAvg
-        
+
         result = OrderedDict(sorted(result.items(), key=lambda t: t[0]))
         # result = sorted(result)
 
@@ -200,12 +222,77 @@ class Mat(object):
             for r in range(self.rowCount):
                 for c in range(r, self.colCount):
                     if r != c and self.values[r][c] == 1:
-                        file.write("{0};{1}\n".format(r,c))
+                        file.write("{0};{1}\n".format(r, c))
 
     def insert(self, other):
         if self.colCount < other.colCount or self.rowCount < other.colCount:
             raise Exception("Cannot insert bigger matrix into smaller one.")
-        
+
         for row in range(other.rowCount):
             for col in range(other.colCount):
                 self.values[row][col] = other.values[row][col]
+
+    def bfs(self, source, dest):
+        visited = [False for c in range(self.colCount)]
+        prev = [-1 for c in range(self.colCount)]
+        current = None
+
+        q = Queue()
+        q.put(source)
+        neib = []
+
+        found = False
+        while not q.empty():
+            current = q.get()
+            if current == dest:
+                found = True
+                break
+
+            if current >= self.rowCount:
+                visited[current] = True
+                continue
+
+            neib = self.get_neighbours(current)
+            for n in neib:
+                if not visited[n]:
+                    q.put(n)
+                    visited[n] = True
+                    prev[n] = current
+
+        if not found:
+            return inf
+
+        path = 0
+        current = dest
+        while current != source:
+            path += 1
+            current = prev[current]
+
+        return path
+
+    def one_mode_projection_for_bipartite_graph(self):
+        # Left bipartite side
+        leftMatValues = [[0 for c in range(self.rowCount)]
+                         for r in range(self.rowCount)]
+        rightMatValues = [[0 for c in range(self.colCount)]
+                          for r in range(self.colCount)]
+
+        for vertex in range(self.rowCount):
+            for otherVertex in range(self.rowCount):
+                if otherVertex == vertex:
+                    continue
+                pathDistance = self.bfs(vertex, otherVertex)
+                if pathDistance <= 2:
+                    leftMatValues[vertex][otherVertex] = 1
+
+        for vertex in range(self.colCount):
+            for otherVertex in range(self.colCount):
+                if otherVertex == vertex:
+                    continue
+                pathDistance = self.bfs(vertex, otherVertex)
+                if pathDistance != inf:
+                    rightMatValues[vertex][otherVertex] = 1
+
+        leftMat = Mat(leftMatValues, self.labels)
+        rightMat = Mat(rightMatValues)
+        return (leftMat, rightMat)
